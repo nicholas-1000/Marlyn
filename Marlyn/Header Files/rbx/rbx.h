38c189cf9e18c1560ac2dc1bfe8f69edbe3c4933
nicholas <75744555+nickisntreal@users.noreply.github.com>
2022-01-30 23:47:26 -0600

    started rbx file

--- Marlyn/Header Files/rbx/rbx.h
+++ Marlyn/Header Files/rbx/rbx.h
@@ -12,6 +12,21 @@ namespace rbx
 
 	namespace marlyn_functions
 	{
-		// test
+		
+	}
+
+	void scan_game()
+	{
+		//visualengine : RobloxPlayerBeta.exe+3222C7C (bashar engine)
+		//datamodel : RobloxPlayerBeta.exe+323B71C	(bashar engine)
+
+		std::cout << GetModuleHandleA(nullptr) << std::endl; /* so we can see the base address of roblox */
+
+		long long datamodel = 0;
+		long long visualengine = 0;
+
+		/* could probably do this another way, but this works fine for an open source project (NOT RELIABLE) */
+		ReadProcessMemory(GetCurrentProcess(), (LPCVOID)reinterpret_cast<std::uint32_t>(GetModuleHandleA(nullptr) + 0x323B71C + 0x0), &datamodel, sizeof(datamodel), 0);
+		ReadProcessMemory(GetCurrentProcess(), (LPCVOID)reinterpret_cast<std::uint32_t>(GetModuleHandleA(nullptr) + 0x3222C7C + 0x0), &visualengine, sizeof(visualengine), 0);
 	}
 }
